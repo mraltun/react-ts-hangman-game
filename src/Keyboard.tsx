63438cb21c3f04a346df0c8a57008773eac9d5bb
Murat Altun <mraltun@gmail.com>
2022-11-04 15:16:28 +0300

    Added props to Keyboard

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -30,7 +30,17 @@ const KEYS = [
   "z",
 ];
 
-const Keyboard = () => {
+type KeyboardProps = {
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+};
+
+const Keyboard = ({
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+}: KeyboardProps) => {
   return (
     <div
       style={{
@@ -40,8 +50,18 @@ const Keyboard = () => {
       }}
     >
       {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key);
+        const isInactive = inactiveLetters.includes(key);
+
         return (
-          <button className={`${styles.btn}`} key={key}>
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInactive ? styles.inactive : ""
+            }`}
+            disabled={isActive || isInactive}
+            key={key}
+          >
             {key}
           </button>
         );
