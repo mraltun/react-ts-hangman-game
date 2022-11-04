88a201e4aeeaa8305441d85d9ce9afe8bde699bb
Murat Altun <mraltun@gmail.com>
2022-11-04 14:32:40 +0300

    Added HangmanDrawing props

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ const App = () => {
     return words[Math.floor(Math.random() * words.length)];
   });
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
 
   return (
     <div
@@ -22,7 +25,7 @@ const App = () => {
       }}
     >
       <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
-      <HangmanDrawing />
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <HangmanWord />
       <div style={{ alignSelf: "stretch" }}>
         <Keyboard />
--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -87,9 +87,16 @@ const LEFT_LEG = (
   />
 );
 
-const HangmanDrawing = () => {
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number;
+};
+
+const HangmanDrawing = ({ numberOfGuesses }: HangmanDrawingProps) => {
   return (
     <div style={{ position: "relative" }}>
+      {BODY_PARTS.slice(0, numberOfGuesses)}
       <div
         style={{
           height: "50px",
@@ -117,12 +124,6 @@ const HangmanDrawing = () => {
         }}
       />
       <div style={{ height: "10px", width: "250px", background: "black" }} />
-      {HEAD}
-      {BODY}
-      {LEFT_ARM}
-      {RIGHT_ARM}
-      {LEFT_LEG}
-      {RIGHT_LEG}
     </div>
   );
 };
