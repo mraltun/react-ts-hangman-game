4518425c7c1acd40df493e62ca44286934748f8d
Murat Altun <mraltun@gmail.com>
2022-11-04 13:26:45 +0300

    HangmanWord logic is done with temporary variables

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const HangmanWord = () => {
-  return <div>HangmanWord</div>;
+  const word = "test";
+  const guessedLetters = ["t"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default HangmanWord;
